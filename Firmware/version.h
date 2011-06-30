2d4778d7d2f961deb9d94e57b13ba5c221a96657
atarity <atarity@gmail.com>
2011-06-30 23:49:32 +0400

    Software: GUI elements are arranged. Some translation are fixed. Again.

---
+++ Firmware/version.h
@@ -0,0 +1,46 @@
+/*
+ * version.h
+ *
+ *  Created on: 03.11.2010
+ *      Author: Mike Shatohin (brunql)
+ *     Project: Lightpack
+ *
+ *  Lightpack is a content-appropriate ambient lighting system for any computer
+ *
+ *  Copyright (c) 2011 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef VERSION_H_INCLUDED
+#define VERSION_H_INCLUDED
+
+// Firmware version:
+#include "../CommonHeaders/LIGHTPACK_HW.h"
+
+#if(LIGHTPACK_HW == 5)
+#define VERSION_OF_FIRMWARE              (0x0500UL)
+#else
+#define VERSION_OF_FIRMWARE              (0x0403UL)
+#endif
+
+#define VERSION_OF_FIRMWARE_MAJOR        ((VERSION_OF_FIRMWARE & 0xff00) >> 8)
+#define VERSION_OF_FIRMWARE_MINOR         (VERSION_OF_FIRMWARE & 0x00ff)
+
+
+// Use Bit Angle Modulation (BAM) instead of Pulse Width Modulation (PWM)
+#define USE_BAM 0
+
+#endif /* VERSION_H_INCLUDED */
---
+++ Software/inc/mainwindow.h
@@ -0,0 +1,160 @@
+/*
+ * mainwindow.h
+ *
+ *  Created on: 26.07.2010
+ *      Author: Mike Shatohin (brunql)
+ *     Project: Lightpack
+ *
+ *  Lightpack is very simple implementation of the backlight for a laptop
+ *
+ *  Copyright (c) 2010, 2011 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui>
+#include "aboutdialog.h"            /* About dialog */
+#include "settings.h"               /* QSettings */
+#include "ambilightusb.h"           /* class AmbilightUsb */
+#include "grabmanager.h"            /* class GrabManager */
+#include "struct_rgb.h"
+#include "speedtest.h"
+#include "../src/qcolorbutton.hpp"
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void startAmbilight();
+
+signals:
+    void settingsProfileChanged();
+
+
+public slots:
+    void ledDeviceCallSuccess(bool isSuccess);
+    void refreshAmbilightEvaluated(double updateResultMs);
+
+protected:
+    virtual void changeEvent(QEvent *e);
+    virtual void closeEvent(QCloseEvent *event);
+    virtual void paintEvent(QPaintEvent * event);
+
+private slots:
+    void on_horizontalSlider_Speed_valueChanged(int value);
+    void on_horizontalSlider_Brightness_valueChanged(int value);
+    void onMoodLampModeChanged(bool isConstantColor);
+    void on_cb_Modes_currentIndexChanged(int index);
+    void iconActivated(QSystemTrayIcon::ActivationReason reason);
+    void showAbout(); /* using in actions */
+    void showSettings(); /* using in actions */
+    void hideSettings(); /* using in iconActivated(..) */
+    void ambilightOn(); /* using in actions */
+    void ambilightOff(); /* using in actions */
+    void quit(); /* using in actions */
+
+    void grabAmbilightOnOff();
+
+    void settingsHardwareTimerOptionsChange();
+    void settingsHardwareSetColorDepth(int value);
+    void settingsHardwareSetSmoothSlowdown(int value);
+    void settingsHardwareSetBrightness(int value);
+
+    void openCurrentProfile();
+
+    void profileRename();
+    void profileSwitch(const QString & configName);
+    void profileTraySwitch();
+    void profileNew();
+    void profileResetToDefaultCurrent();
+    void profileDeleteCurrent();
+    void settingsProfileChanged_UpdateUI();
+
+    void loadTranslation(const QString & language);
+
+    void switchQtWinAPIClick();
+    void startTestsClick();
+    void updateGrabbedColors(const QList<StructRGB> & colors);
+
+    void setAvgColorOnAllLEDs(int value);
+
+    void onMoodLampColorChanged(QColor color);
+
+private:
+    void connectSignalsSlots();
+
+    void updateTrayAndActionStates();
+
+    void createTrayIcon();
+    void createActions();
+    void loadSettingsToMainWindow();
+
+    void grabSwitchQtWinAPI();
+
+    void profilesFindAll();
+    void profileLoadLast();
+    void profileTraySync();
+
+    void initLanguages();
+
+    void openFile(const QString &filePath);
+
+    void updatePwmFrequency();
+
+    void initLabelsForGrabbedColors();
+
+    void updateCbModesPosition();
+
+private:
+    ILedDevice *ledDevice;
+    GrabManager *grabManager;
+    AboutDialog *aboutDialog;
+    SpeedTest *speedTest;
+
+    bool isAmbilightOn; /* is grab desktop window ON */
+    bool isErrorState;
+    bool isWinAPIGrab;
+
+    // Evaluated frequency of the PWM generation
+    double pwmFrequency;
+
+    QList<QLabel *> labelsGrabbedColors;
+
+    Ui::MainWindow *ui;
+
+    QAction *onAmbilightAction;
+    QAction *offAmbilightAction;
+    QAction *settingsAction;
+    QAction *aboutAction;
+    QAction *quitAction;
+
+    QSystemTrayIcon *trayIcon;
+    QMenu *trayIconMenu;
+    QMenu *profilesMenu;
+
+    QTranslator *translator;
+};
+
+#endif // MAINWINDOW_H
