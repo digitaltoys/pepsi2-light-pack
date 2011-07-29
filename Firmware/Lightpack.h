f903218d1c1f3e861686f4c682b54f2e8dc317b1
Eraser <eraser1981@googlemail.com>
2011-07-29 15:29:21 +0400

    Software: Fixes Issue 107

---
+++ Firmware/Lightpack.h
@@ -0,0 +1,60 @@
+/*
+ * Lightpack.h
+ *
+ *  Created on: 11.01.2011
+ *      Author: Mike Shatohin (brunql)
+ *     Project: Lightpack
+ *
+ *  Lightpack is a content-appropriate ambient lighting system for any computer
+ *
+ *  Copyright (c) 2011 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef LIGHTPACK_H_INCLUDED
+#define LIGHTPACK_H_INCLUDED
+
+/* Includes: */
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include <util/delay.h>
+
+#include "Descriptors.h"
+
+#include <LUFA/Version.h>
+#include <LUFA/Drivers/Board/LEDs.h>
+#include <LUFA/Drivers/USB/USB.h>
+
+#include "flags.h"
+#include "iodefs.h"
+#include "datatypes.h"
+#include "version.h"
+
+#include "../CommonHeaders/LEDS_COUNT.h"
+
+/* LED use with I/O manipulations macroses from iodefs.h */
+#define LEDR    (D,2)
+#define LEDW    (D,3)
+
+// Global variables
+extern Settings_t g_Settings;
+extern Images_t g_Images;
+
+#endif /* LIGHTPACK_H_INCLUDED */
+
